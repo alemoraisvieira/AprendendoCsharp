cc99c99f23e69d049a64f065706a636c44577e17
alemoraisvieira <62805016+alemoraisvieira@users.noreply.github.com>
2020-04-01 21:32:29 -0300

    alteraçoes

---
+++ AprendendoCSharp/4-ConversoesNumericas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesNumericas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            salario = 1200.50;
+
+            int salarioInteiro;
+            salarioInteiro = (int)salario;
+
+            Console.WriteLine("salario inteiro é" + salarioInteiro);
+
+            long idade;
+            idade = 2000000000000000000;
+            Console.WriteLine(idade);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char primeiraLetra;
+            primeiraLetra ='A';
+            Console.WriteLine(primeiraLetra);
+            Console.ReadLine();
+
+
+        }
+    }
+}
