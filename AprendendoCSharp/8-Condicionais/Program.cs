3105606cbde542872a3be08bb7772ad707e1fe72
alemoraisvieira <62805016+alemoraisvieira@users.noreply.github.com>
2020-04-14 21:57:19 -0300

    operadores logicos

---
+++ AprendendoCSharp/8-Condicionais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("executando projeto 8 - condicionais");
+
+            int IdadeJoao = 16;
+            int QuantidadePessoas = 2;
+
+            if (IdadeJoao >= 18)
+            {
+                Console.WriteLine("João possui mais que 18 anos");
+            }
+            else
+            {
+                if (QuantidadePessoas >= 2)
+                {
+                    Console.WriteLine("Joao possui menos que 18 anos e a quantidade de pessoas é >=2");
+                }
+                else 
+                {
+                    Console.WriteLine("Joao possui menos que 18 anos e a quantidade de pessoas é <2");
+                }
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ AprendendoCSharp/8.2-Variavel Booleana/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("executando projeto 8 - condicionais");
+
+            int IdadeJoao = 16;
+            int QuantidadePessoas = 2;
+
+            if (IdadeJoao >= 18 || QuantidadePessoas >= 2)
+            {
+                Console.WriteLine("João pode entrar");
+            }
+            else
+            {
+
+                Console.WriteLine("Joao não pode entrar");
+            }
+
+
+        }
+    }
+}
