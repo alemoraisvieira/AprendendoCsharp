6ab5a40360dc14ab6ade1e4c5faed641d7b44a55
alemoraisvieira <62805016+alemoraisvieira@users.noreply.github.com>
2020-04-18 19:30:57 -0300

    laço de repetição

---
+++ AprendendoCSharp/11-LacoRepeticaoFor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11_LacoRepeticaoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+
+            {
+                valorInvestido = valorInvestido * 1.0036;
+                Console.WriteLine(
+                    "Após" + contadorMes +
+                    "meses, você terá R$" + valorInvestido
+                    );
+            }
+            Console.ReadLine();
+        }
+    }
+}
